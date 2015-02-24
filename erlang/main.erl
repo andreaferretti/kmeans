f6d38bc37ff532290bfc5e0bee6d35686dcab8fe
Hynek Vychodil <vychodil.hynek@gmail.com>
2015-02-24 17:40:02 +0100

    Use more idiomatic Erlang

--- erlang/main.erl
+++ erlang/main.erl
@@ -7,27 +7,21 @@ n() -> 10.
 iters() -> 15.
 
 run() ->
-	{ok, Device} = file:open("../points.json", [read]),
-	Line = io:get_line(Device, noLine),
-	Tokens = string:tokens(Line, ",[]"),
-	Xs = getFloats(Tokens),
-	Times = lists:seq(1, times()),
-	Sums = [getTime(Xs) || _ <- Times],
-	Time = lists:sum(Sums)/length(Times),
+	Xs = read_points("../points.json"),
+	Times = [getTime(Xs) || _ <- lists:seq(1, times())],
+	Time = lists:sum(Times)/times(),
 	Time.
 
+read_points(FileName) ->
+    {ok, Data} = file:read_file(FileName),
+    [<<>>|Bins] = re:split(Data, "[][,]+", [trim]),
+    points([binary_to_float(X) || X <- Bins]).
+
 getTime(Xs) ->
-	TimeBefore = get_timestamp(),
-	kmeans:run(Xs, n(), iters()),
-	TimeAfter = get_timestamp(),
-	TimeAfter - TimeBefore.
+    Ms = element(1, timer:tc(kmeans, run, [Xs, n(), iters()]))/1000,
+%    io:write(Ms), io:nl(),
+    Ms.
 
-getFloats([]) -> [];
-getFloats([H1, H2 | T]) ->
-	[F1 | _ ] = tuple_to_list(string:to_float(H1)),
-	[F2 | _ ] = tuple_to_list(string:to_float(H2)),
-	[{F1, F2} | getFloats(T)].
-
-get_timestamp() ->
-	{Mega, Sec, Micro} = os:timestamp(),
-	(Mega*1000000 + Sec)*1000 + round(Micro/1000). 
+points([]) -> [];
+points([H1, H2 | T]) ->
+	[{H1, H2} | points(T)].
