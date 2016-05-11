cb8f7b6b037677611970dce2af60392a4b8c8b0e
Andrea Ferretti <ferrettiandrea@gmail.com>
2016-05-11 15:08:13 +0200

    Improved utils, kmeans cleanup

--- opencl/kmeans.nim
+++ opencl/kmeans.nim
@@ -19,58 +19,38 @@ proc main() =
     context = devices.createContext
     program = context.createProgram(body)
     queue = context.commandQueueFor(devices[0])
-    pointSize = points.len * sizeof(Point)
-    centroidSize = centroids.len * sizeof(Centroid)
     workGroups = devices[0].maxWorkGroups
     workItems = (points.len div workGroups).nextPowerOfTwo
-    parallelism = workItems * workGroups
-    accumSize = centroids.len * workGroups * sizeof(Accum)
+
   program.buildOn(devices)
 
   let
     groupByCluster = program.createKernel("group_by_cluster")
     sumPoints = program.createKernel("sum_points")
     updateCentroids = program.createKernel("update_centroids")
+    start = cpuTime()
+    gpuPoints = context.buffer(points)
+    gpuCentroids = context.buffer(centroids)
+    gpuAccum = buffer[Accum](context, centroids.len * workGroups)
 
-  let start = cpuTime()
-
-  var status: TClResult
-  var gpuPoints = createBuffer(context, MEM_READ_WRITE, pointSize, nil, addr status)
-  check status
-  var gpuCentroids = createBuffer(context, MEM_READ_WRITE, centroidSize, nil, addr status)
-  check status
-  var gpuAccum = createBuffer(context, MEM_READ_WRITE, accumSize, nil, addr status)
-  check status
-
-  groupByCluster.bindArgs([gpuPoints, gpuCentroids], [points.len.int32, centroids.len.int32])
-  var
-    numPoints = points.len.int32
-    numCentroids = centroids.len.int32
-  check setKernelArg(sumPoints, 0, sizeof(Pmem), addr gpuPoints)
-  check setKernelArg(sumPoints, 1, sizeof(Pmem), addr gpuAccum)
-  check setKernelArg(sumPoints, 2, workItems * centroids.len * sizeof(Accum), nil)
-  check setKernelArg(sumPoints, 3, sizeof(int32), addr numPoints)
-  check setKernelArg(sumPoints, 4, sizeof(int32), addr numCentroids)
-  updateCentroids.bindArgs([gpuAccum, gpuCentroids], [workGroups.int32, centroids.len.int32])
+  groupByCluster.args(gpuPoints, gpuCentroids, points.len.int32, centroids.len.int32)
+  sumPoints.args(gpuPoints, gpuAccum, LocalBuffer[Accum](centroids.len * workItems), points.len.int32, centroids.len.int32)
+  updateCentroids.args(gpuAccum, gpuCentroids, workGroups.int32, centroids.len.int32)
 
   for _ in 1 .. iterations:
     for i in 0 .. < centroids.len:
       centroids[i].x = points[i].x
       centroids[i].y = points[i].y
 
-    queue.write(addr points[0], gpuPoints, pointSize)
-    queue.write(addr centroids[0], gpuCentroids, centroidSize)
+    queue.write(points, gpuPoints)
+    queue.write(centroids, gpuCentroids)
 
     for _ in 1 .. 15:
       queue.run(groupByCluster, points.len)
-      queue.run(sumPoints, parallelism, workItems)
+      queue.run(sumPoints, workItems * workGroups, workItems)
       queue.run(updateCentroids, centroids.len)
 
-    var accums = newSeq[Accum](centroids.len * workGroups)
-
-    queue.read(addr points[0], gpuPoints, pointSize)
-    queue.read(addr centroids[0], gpuCentroids, centroidSize)
-    queue.read(addr accums[0], gpuAccum, accumSize)
+    queue.read(centroids, gpuCentroids)
 
   let time = (((cpuTime() - start) * 1000) / float(iterations)).round
   echo format("Made $1 iterations with an average of $2 milliseconds",
@@ -80,14 +60,14 @@ proc main() =
     echo a
 
   # Clean up
-  check releaseCommandQueue(queue)
-  check releaseKernel(groupByCluster)
-  check releaseKernel(sumPoints)
-  check releaseKernel(updateCentroids)
-  check releaseProgram(program)
-  check releaseMemObject(gpuPoints)
-  check releaseMemObject(gpuCentroids)
-  check releaseContext(context)
+  release(queue)
+  release(groupByCluster)
+  release(sumPoints)
+  release(updateCentroids)
+  release(program)
+  release(gpuPoints)
+  release(gpuCentroids)
+  release(context)
 
 when isMainModule:
   main()
--- opencl/util.nim
+++ opencl/util.nim
@@ -18,10 +18,32 @@ proc name*(id: PPlatformId): string =
   result = newString(size)
   check getPlatformInfo(id, PLATFORM_NAME, size, addr result[0], nil)
 
+proc name*(id: PDeviceId): string =
+  var size = 0
+  check getDeviceInfo(id, DEVICE_NAME, 0, nil, addr size)
+  result = newString(size)
+  check getDeviceInfo(id, DEVICE_NAME, size, addr result[0], nil)
 
 proc maxWorkGroups*(id: PDeviceId): int =
   check getDeviceInfo(id, DEVICE_MAX_WORK_GROUP_SIZE, sizeof(int), addr result, nil)
 
+proc localMemory*(id: PDeviceId): uint64 =
+  check getDeviceInfo(id, DEVICE_LOCAL_MEM_SIZE, sizeof(int), addr result, nil)
+
+proc globalMemory*(id: PDeviceId): uint64 =
+  check getDeviceInfo(id, DEVICE_GLOBAL_MEM_SIZE, sizeof(int), addr result, nil)
+
+proc maxWorkItems*(id: PDeviceId): seq[int] =
+  var dims: int
+  check getDeviceInfo(id, DEVICE_MAX_WORK_ITEM_DIMENSIONS, sizeof(int), addr dims, nil)
+  result = newSeq[int](dims)
+  check getDeviceInfo(id, DEVICE_MAX_WORK_ITEM_SIZES, dims * sizeof(int), addr result[0], nil)
+
+proc version*(id: PPlatformId): string =
+  var size = 0
+  check getPlatformInfo(id, PLATFORM_VERSION, 0, nil, addr size)
+  result = newString(size)
+  check getPlatformInfo(id, PLATFORM_VERSION, size, addr result[0], nil)
 
 proc getPlatformByName*(platformName: string): PPlatformId =
   var numPlatforms: uint32
@@ -57,9 +79,23 @@ proc createProgram*(context: PContext, body: string): PProgram =
   result = createProgramWithSource(context, 1, cast[cstringArray](addr lines), nil, addr status)
   check status
 
+proc buffer*[A](context: PContext, size: int, flags: Tmem_flags = MEM_READ_WRITE): PMem =
+  var status: TClResult
+  result = createBuffer(context, flags, size * sizeof(A), nil, addr status)
+  check status
+
+proc buffer*[A](context: PContext, xs: seq[A], flags: Tmem_flags = MEM_READ_WRITE): PMem =
+  buffer[A](context, xs.len, flags)
+
 proc buildOn*(program: PProgram, devices: seq[PDeviceId]) =
   var devs = devices
   check buildProgram(program, devs.len.uint32, cast[ptr PDeviceId](addr devs[0]), nil, nil, nil)
+
+proc buildErrors*(program: PProgram, devices: seq[PDeviceId]): string =
+  var logSize: int
+  check getProgramBuildInfo(program, devices[0], PROGRAM_BUILD_LOG, 0, nil, addr logSize)
+  result = newString(logSize + 1)
+  check getProgramBuildInfo(program, devices[0], PROGRAM_BUILD_LOG, logSize, addr result[0], nil)
 
 proc commandQueueFor*(context: PContext, device: PDeviceId): PCommandQueue =
   var status: TClResult
@@ -71,16 +107,59 @@ proc createKernel*(program: PProgram, name: string): PKernel =
   result = createKernel(program, name, addr status)
   check status
 
-proc bindArgs*(kernel: PKernel, buffers: openarray[PMem], ints: openarray[int32]) =
-  var bufs = @buffers
-  var intBufs = @ints
-  for i in 0 .. < bufs.len:
-    check setKernelArg(kernel, i.uint32, sizeof(Pmem), addr bufs[i])
-  for i in 0 .. < intBufs.len:
-    check setKernelArg(kernel, (bufs.len + i).uint32, sizeof(int32), addr intBufs[i])
+type
+  LocalBuffer*[A] = distinct int
+  anyInt = int or int32 or int64
 
-proc run*(queue: PCommandQueue, kernel: PKernel, parallelism: int) =
-  var globalWorkSize = [parallelism, 0, 0]
+template setArg(kernel: PKernel, item: PMem, index: int) =
+  var x = item
+  check setKernelArg(kernel, index.uint32, sizeof(Pmem), addr x)
+
+template setArg[A](kernel: PKernel, item: var A, index: int) =
+  check setKernelArg(kernel, index.uint32, sizeof(A), addr item)
+
+template setArg[A](kernel: PKernel, item: LocalBuffer[A], index: int) =
+  check setKernelArg(kernel, index.uint32, int(item) * sizeof(A), nil)
+
+template setArg(kernel: PKernel, item: anyInt, index: int) =
+  var x = item
+  check setKernelArg(kernel, index.uint32, sizeof(type(item)), addr x)
+
+proc args*[A1](kernel: PKernel, a1: A1) =
+  kernel.setArg(a1, 0)
+
+proc args*[A1, A2](kernel: PKernel, a1: A1, a2: A2) =
+  kernel.setArg(a1, 0)
+  kernel.setArg(a2, 1)
+
+proc args*[A1, A2, A3](kernel: PKernel, a1: A1, a2: A2, a3: A3) =
+  kernel.setArg(a1, 0)
+  kernel.setArg(a2, 1)
+  kernel.setArg(a3, 2)
+
+proc args*[A1, A2, A3, A4](kernel: PKernel, a1: A1, a2: A2, a3: A3, a4: A4) =
+  kernel.setArg(a1, 0)
+  kernel.setArg(a2, 1)
+  kernel.setArg(a3, 2)
+  kernel.setArg(a4, 3)
+
+proc args*[A1, A2, A3, A4, A5](kernel: PKernel, a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) =
+  kernel.setArg(a1, 0)
+  kernel.setArg(a2, 1)
+  kernel.setArg(a3, 2)
+  kernel.setArg(a4, 3)
+  kernel.setArg(a5, 4)
+
+proc args*[A1, A2, A3, A4, A5, A6](kernel: PKernel, a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) =
+  kernel.setArg(a1, 0)
+  kernel.setArg(a2, 1)
+  kernel.setArg(a3, 2)
+  kernel.setArg(a4, 3)
+  kernel.setArg(a5, 4)
+  kernel.setArg(a6, 5)
+
+proc run*(queue: PCommandQueue, kernel: PKernel, totalWork: int) =
+  var globalWorkSize = [totalWork, 0, 0]
   check enqueueNDRangeKernel(queue, kernel, 1, nil,  cast[ptr int](addr globalWorkSize), nil, 0, nil, nil)
 
 proc run*(queue: PCommandQueue, kernel: PKernel, totalWork, localWork: int) =
@@ -92,5 +171,17 @@ proc run*(queue: PCommandQueue, kernel: PKernel, totalWork, localWork: int) =
 proc write*(queue: PCommandQueue, src: pointer, dest: PMem, size: int) =
   check enqueueWriteBuffer(queue, dest, CL_FALSE, 0, size, src, 0, nil, nil)
 
+proc write*[A](queue: PCommandQueue, src: var seq[A], dest: PMem) =
+  write(queue, addr src[0], dest, src.len * sizeof(A))
+
 proc read*(queue: PCommandQueue, dest: pointer, src: PMem, size: int) =
   check enqueueReadBuffer(queue, src, CL_TRUE, 0, size, dest, 0, nil, nil)
+
+proc read*[A](queue: PCommandQueue, dest: var seq[A], src: PMem) =
+  read(queue, addr dest[0], src, dest.len * sizeof(A))
+
+template release*(queue: PCommandQueue) = check releaseCommandQueue(queue)
+template release*(kernel: PKernel) = check releaseKernel(kernel)
+template release*(program: PProgram) = check releaseProgram(program)
+template release*(buffer: PMem) = check releaseMemObject(buffer)
+template release*(context: PContext) = check releaseContext(context)
