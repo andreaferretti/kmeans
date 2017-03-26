30f1e4704b727270273b0901b35b16b1e9ec1af2
Pascal Costanza <pc@p-cos.net>
2017-03-26 19:17:06 +0200

    Improved performance of the Go version, and made it more idiomatic as well.

--- go/main.go
+++ go/main.go
@@ -8,84 +8,86 @@ import (
 	"time"
 )
 
-type Point struct {
-	x, y float64
-}
-
-func (p *Point) add(p2 *Point) *Point {
-	return &Point{p.x + p2.x, p.y + p2.y}
-}
-func (p *Point) sub(p2 *Point) *Point {
-	return &Point{p.x - p2.x, p.y - p2.y}
-}
-func (p *Point) divide(d float64) *Point {
-	return &Point{p.x / d, p.y / d}
-}
-func (p *Point) modulus() float64 {
-	return math.Sqrt(sq(p.x) + sq(p.y))
-}
 
 func sq(x float64) float64 {
 	return x * x
 }
 
-func dist(p1 *Point, p2 *Point) float64 {
+type Point struct {
+	x, y float64
+}
+
+func (p Point) add(p2 Point) Point {
+	return Point{p.x + p2.x, p.y + p2.y}
+}
+func (p Point) sub(p2 Point) Point {
+	return Point{p.x - p2.x, p.y - p2.y}
+}
+func (p Point) divide(d float64) Point {
+	return Point{p.x / d, p.y / d}
+}
+func (p Point) modulus() float64 {
+	return math.Sqrt(sq(p.x) + sq(p.y))
+}
+
+func dist(p1 Point, p2 Point) float64 {
 	return (p1.sub(p2)).modulus()
 }
 
-func average(points []Point) *Point {
+func average(points []Point) Point {
 	tmp := Point{0, 0}
-	for i := range points {
-		tmp = *tmp.add(&points[i])
+	for _, point := range points {
+		tmp = tmp.add(point)
 	}
-	tmp = *tmp.divide(float64(len(points)))
-	return &tmp
+	tmp = tmp.divide(float64(len(points)))
+	return tmp
 }
 
-func closest(p *Point, choices []Point) *Point {
+func closest(p Point, choices []Point) int {
 	min := 0
-	minDist := 0.0
-	for i := range choices {
-		actualDist := dist(p, &choices[i])
-		if i == 0 || minDist > actualDist {
+	minDist := math.MaxFloat64
+	for i, choice := range choices {
+		actualDist := dist(p, choice)
+		if minDist > actualDist {
 			min = i
 			minDist = actualDist
 		}
 	}
-	return &choices[min]
+	return min
 }
 
 func clusters(xs []Point, centroids []Point) [][]Point {
-	hm := make(map[Point][]Point)
-	for i := range xs {
-		theClosest := *closest(&xs[i], centroids)
-		hm[theClosest] = append(hm[theClosest], xs[i])
+	hm := make([][]Point, len(centroids))
+	for _, x := range xs {
+		theClosest := closest(x, centroids)
+		hm[theClosest] = append(hm[theClosest], x)
 	}
-	result := [][]Point{}
-
-	for _, val := range hm {
-		result = append(result, val)
+	i := 0
+	for i < len(hm) {
+		if len(hm[i]) > 0 {
+			i++
+		} else {
+			hm = append(hm[:i], hm[i+1:]...)
+		}
 	}
-	return result
+	return hm
 }
 
 func run(n int, iters int, xs []Point) [][]Point {
-	centroids := []Point{}
-
-	for i := 0; i < n; i++ {
-		centroids = append(centroids, xs[i])
-	}
+	centroids := make([]Point, n)
+	copy(centroids, xs)
 
 	for k := 0; k < iters; k++ {
 		clus := clusters(xs, centroids)
 		for i := 0; i < n; i++ {
-			centroids[i] = *average(clus[i])
+			centroids[i] = average(clus[i])
 		}
 	}
 
-	/*fmt.Println("Final Centroids are ")
-		for i:=0; i < n; i++ {
-			fmt.Println(centroids[i])
+	/*
+	fmt.Println("Final Centroids are ")
+	for _, centroid := range centroids {
+		fmt.Println(centroid)
 	}*/
 
 	return clusters(xs, centroids)
@@ -109,8 +111,8 @@ func main() {
 
 	xs := []Point{}
 
-	for i := range *res {
-		xs = append(xs, Point{(*res)[i][0], (*res)[i][1]})
+	for _, res := range *res {
+		xs = append(xs, Point{res[0], res[1]})
 	}
 
 	before := time.Now()
